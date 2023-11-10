1801d1c2f47c2f69a977e410734ff35392c1e8f7
sarahmohamed73 <sarahmoab73@gmail.com>
2023-11-11 01:32:07 +0200

    Design 404 Page & Spinner & Some Changes

--- frontend/src/Components/AboutUs/Sliderr.js
+++ frontend/src/Components/AboutUs/Sliderr.js
@@ -6,9 +6,9 @@ import React from 'react'
 
 const SlideContent = ({ image, title }) => {
     return (
-       <div className="slider-container p-0 m-0">
+       <div className="slider-container p-0 m-0 ">
         <div className="slider-text">Travel brings power and love back into your life .</div>
-        <img src={image} alt={title} className="slider-image " />
+        <img src={image} alt={title} className="slider-image w-100" />
         <h3>{title}</h3>
       </div>
     );
--- frontend/src/Pages/404.js
+++ frontend/src/Pages/404.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Error404 from './../Assets/Images/404.gif'
+import { NavLink } from 'react-router-dom'
 
 export default function Error() {
   return (
-    <div>
-      <h1>Error</h1>
+    <div  style={{height: "500px"}}>
+      <img className='h-75' src={Error404} />
+      <br />
+      <NavLink className="mt-4" to="/"> &larr; Back To Home</NavLink>
     </div>
   )
 }
