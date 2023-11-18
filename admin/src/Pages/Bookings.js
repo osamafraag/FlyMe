0934e6dc8407d4f727faa0fe8b233e5f9c41d1cc
osamamagar <osamamagar99@gmail.com>
2023-11-18 21:01:47 +0200

    added Booking in admin dashBoard

--- admin/src/Pages/Bookings.js
+++ admin/src/Pages/Bookings.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Bookings from '../Components/Bookings/Bookings'
 
-export default function Bookings() {
+export default function Booking() {
   return (
     <div className='container p-5'>
       <h3 className='text-start text-secondary'>Bookings</h3>
+      <Bookings/>
     </div>
   )
 }
