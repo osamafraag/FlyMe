585bf1397d77f3c6df3d1c26d0fe25fc6828d0f7
sarakamalsaraya <sarakamalsaraya@gmail.com>
2023-11-20 13:24:27 +0200

    add flight history to profile

--- frontend/src/APIs/UserFlightHistory.js
+++ frontend/src/APIs/UserFlightHistory.js
@@ -1,6 +1,6 @@
 import { axiosInstance } from "./Config";
 
-export const UserFlightHistory = (userID) => {
+export const UserFlightHistory = (userID, headers) => {
   
-  return axiosInstance.get(`flights/api/user/${userID}/history/`)
+  return axiosInstance.get(`flights/api/user/${userID}/history/`, headers)
 }
