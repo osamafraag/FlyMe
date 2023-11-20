bdb0352b95e640fb11ef4683d1adf4e804cf11a1
sarahmohamed73 <sarahmoab73@gmail.com>
2023-11-20 22:50:40 +0200

    Handel Error In Edit Profile, Cancel Book

--- frontend/src/APIs/FlightData.js
+++ frontend/src/APIs/FlightData.js
@@ -1,5 +1,7 @@
 import { axiosInstance } from "./Config";
 
 export const FlightData = (flightID, headers) => {
-  return axiosInstance.get(`flights/api/${flightID}`, headers)
+  return axiosInstance.get(`flights/api/${flightID}`, {
+    headers:headers
+  })
 }
--- frontend/src/APIs/UserFlightHistory.js
+++ frontend/src/APIs/UserFlightHistory.js
@@ -2,5 +2,7 @@ import { axiosInstance } from "./Config";
 
 export const UserFlightHistory = (userID, headers) => {
   
-  return axiosInstance.get(`flights/api/user/${userID}/history/`, headers)
+  return axiosInstance.get(`flights/api/user/${userID}/history/`, {
+    headers: headers
+  })
 }
